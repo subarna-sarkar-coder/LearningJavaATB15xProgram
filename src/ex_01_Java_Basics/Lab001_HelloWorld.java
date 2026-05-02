b612b68438ac3dbe139ddda52182a89ecaf38f63
Subarna Sarkar_0808 <subarna.sarkar0808@gmail.com>
2026-05-02 11:16:19 +0530

    Add additional print statement for 'Marbo tk'

--- src/ex_01_Java_Basics/Lab001_HelloWorld.java
+++ src/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -4,6 +4,6 @@ public class Lab001_HelloWorld {
     public static void main(String[] args)  //Main gate to execute the code
     {
         System.out.println("Hello World"); //System.out.println to print
-
+        System.out.println("Marbo tk");
     }
 }
