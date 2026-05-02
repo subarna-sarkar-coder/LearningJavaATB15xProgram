14c9408a977c3b1509f9d63618a0ae9510c2635a
Subarna Sarkar_0808 <subarna.sarkar0808@gmail.com>
2026-05-02 11:20:31 +0530

    Update HelloWorld.java output messages

--- src/ex_01_Java_Basics/Lab001_HelloWorld.java
+++ src/ex_01_Java_Basics/Lab001_HelloWorld.java
@@ -3,7 +3,7 @@ package ex_01_Java_Basics;
 public class Lab001_HelloWorld {
     public static void main(String[] args)  //Main gate to execute the code
     {
-        System.out.println("Hello World"); //System.out.println to print
-        System.out.println("Marbo tk");
+        System.out.println("Hello"); //System.out.println to print
+        System.out.println("You are my world");
     }
 }
